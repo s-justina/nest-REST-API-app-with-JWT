bdd3ff4a00fa2ad8240fa30ffecf13e695263416
s-justina <jus.sobcz@gmail.com>
2021-04-06 02:09:28 +0200

    fix bug with dotenv

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersModule } from '../users/users.module';
+import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService, LocalStrategy, JwtStrategy],
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: `${process.env.JWT_SECRET}`,
-      signOptions: { expiresIn: '60s' },
+    JwtModule.registerAsync({
+      useFactory: async () => ({
+        secretOrPrivateKey: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '6000s' },
+      }),
     }),
   ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
